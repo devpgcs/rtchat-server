3444e79f7314ecf14f3bdee97042c2eb4b1730a2
Pedro Castro <devpgcs@hotmail.com>
2024-10-28 13:06:54 -0500

    [Core] Env config added

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,12 @@ import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './chat/message/message.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/rtchat'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL ?? 'mongodb://localhost/rtchat'),
     ChatModule,
     MessageModule,
     UserModule,
